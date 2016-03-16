f338bfb061bc72d1241881f8b86848bc48ac069c
Phil Crissman <phcrissm@microsoft.com>
2016-03-16 06:43:13 -0700

    added person_params

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,10 @@ class PeopleController < ApplicationController
 
   def index
   end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:email, :username, :password, :password_confirmation)
+  end
 end
