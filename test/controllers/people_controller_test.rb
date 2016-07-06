f52ed73da84ae7aeaca59a71239be37634053646
Phil Crissman <phcrissm@microsoft.com>
2016-07-06 09:10:55 -0700

    WIP, making signup work.

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -13,10 +13,18 @@ class PeopleControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "get to new provides a user_sign_up" do
+  test "get to new provides a person_sign_up" do
     get :new
 
     assert assigns(:person).is_a?(Person::SignUp)
+  end
+
+  test "post to create, creates and logs in a person" do
+    post :create, params: {:person => {:email => "george@example.com", :password => "AwesomeSauce", :password_confirmation => "AwesomeSauce"}}
+
+    assert assigns(:person).is_a?(Person::SignUp)
+    assert_equal "george@example.com", assigns(:person).email
+    # assert_redirects
   end
 
 end
